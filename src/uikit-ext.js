f93bbde4cab102927e7c408b5b73d35cb507dec8
peter <peter@kinamo.be>
2021-04-07 10:14:11 +0200

    config project

---
+++ src/uikit-ext.js
@@ -0,0 +1,26 @@
+import _ from 'lodash';
+
+// import UIkit
+
+import UIkit from 'uikit';
+import Icons from 'uikit/dist/js/uikit-icons';
+
+UIkit.use(Icons);
+
+// import JQUERY
+
+import jQuery from "jquery";
+
+const $ = jQuery;
+window.$ = $;
+window.jQuery = jQuery;
+
+// import CSS
+
+import './uikit-ext.scss';
+
+// ready
+
+console.log(_.join(['Hello', 'webpack'], ' '));
+
+export {UIkit, $, _};
---
+++ webpack.config.js
@@ -0,0 +1,82 @@
+const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const devMode = process.env.NODE_ENV !== 'production';
+
+module.exports = {
+    entry: {
+        'uikit-ext': path.resolve(__dirname, 'src/uikit-ext.js'),
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].js',
+        library: '_'
+    },
+    resolve: {
+        extensions: ['.js', '.ts', '.tsx'],
+    },
+    plugins: [
+        // plugin to create index.html file
+        new HtmlWebpackPlugin({
+            title: 'UIkit-ext demo',
+            template: 'src/uikit-ext.pug',
+        }),
+        // plugin to build CSS in a separate file
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+            chunkFilename: '[id].css',
+            ignoreOrder: false, // Enable to remove warnings about conflicting order
+        }),
+    ],
+    optimization: {
+        minimize: devMode === false,
+        minimizer: [
+            new TerserPlugin({}), // plugin to minimize JS
+            new OptimizeCSSAssetsPlugin({}) // plugin to minify CSS
+        ],
+    },
+    module: {
+        rules: [
+            // PUG files
+            {
+                test: /\.pug$/,
+                loader: 'pug-loader'
+            },
+            // CSS files (css, scss)
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                    },
+                    // CSS loader, need this for loading google fonts
+                    'css-loader',
+                    // SASS loader, NOT node-sass,
+                    {
+                        loader: 'sass-loader', options: {
+                            sourceMap: true,
+                            implementation: require('sass'), // not node-sass !
+                        }
+                    }
+                ],
+            },
+            // JS files
+            {
+                test: /\.js/,
+                exclude: /(node_modules|bower_components)/,
+                use: [
+                    // use babel plugin to support older browsers
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env']
+                        }
+                    }
+                ]
+            },
+        ],
+    }
+};
