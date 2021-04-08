e9ad767ea8d13cbba47bc4ccda7a7344596b9a03
peter <peter@kinamo.be>
2021-04-08 16:54:01 +0200

    offcanvas change

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,8 +1,8 @@
 const path = require('path');
 const TerserPlugin = require('terser-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 const devMode = process.env.NODE_ENV !== 'production';
 
